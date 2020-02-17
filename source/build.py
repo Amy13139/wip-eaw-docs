6e7535b8346ef162a0c47eb87c9f8179d9a5117f
luke13139 <59899175+luke13139@users.noreply.github.com>
2020-02-17 09:49:17 -0500

    Setup build.py

---
+++ source/build.py
@@ -0,0 +1,16 @@
+from build_docs import *
+# Doc Generation Script
+# Variables
+eaw_data_dir = path.join(getcwd(), "data\\")
+foc_data_dir = path.join(eaw_data_dir, "corruption\\")
+basegame_data_dir = path.join(getcwd(), "basegame\\")
+xml_data_dir = path.join(basegame_data_dir, "xml")
+
+# Build docs
+print("Building XML Structure...")
+xml.build_xml_structure(
+	path.join(eaw_data_dir, "XML"),
+	path.join(foc_data_dir, "XML"),
+	path.join(xml_data_dir, "xml_structure.rst")
+)
+print("Done Building XML Structure")
--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 from os import path, getcwd
-import build_docs
+from . import build_docs
 
 
 # -- Project information -----------------------------------------------------
@@ -55,18 +55,3 @@ exclude_patterns = []
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# Doc Generation
-# Variables
-eaw_data_dir = path.join(getcwd(), "data\\")
-foc_data_dir = path.join(eaw_data_dir, "corruption\\")
-basegame_data_dir = path.join(getcwd(), "basegame\\")
-xml_data_dir = path.join(basegame_data_dir, "xml")
-
-# Build docs
-print("Building XML Structure...")
-build_docs.xml.build_xml_structure(
-	path.join(eaw_data_dir, "XML"),
-	path.join(foc_data_dir, "XML"),
-	path.join(xml_data_dir, "xml_structure.rst")
-)
-print("Done Building XML Structure")
