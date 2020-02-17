f5d7e8eb7ac9dc3881f78c058586fdf3f687d29c
luke13139 <59899175+luke13139@users.noreply.github.com>
2020-02-17 09:33:35 -0500

    Auto-generation: Set to run when building docs, moved folder, removed older files, added script to repository

--- source/conf.py
+++ source/conf.py
@@ -13,6 +13,8 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+from os import path, getcwd
+import build_docs
 
 
 # -- Project information -----------------------------------------------------
@@ -52,3 +54,19 @@ exclude_patterns = []
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Doc Generation
+# Variables
+eaw_data_dir = path.join(getcwd(), "data\\")
+foc_data_dir = path.join(eaw_data_dir, "corruption\\")
+basegame_data_dir = path.join(getcwd(), "basegame\\")
+xml_data_dir = path.join(basegame_data_dir, "xml")
+
+# Build docs
+print("Building XML Structure...")
+build_docs.xml.build_xml_structure(
+	path.join(eaw_data_dir, "XML"),
+	path.join(foc_data_dir, "XML"),
+	path.join(xml_data_dir, "xml_structure.rst")
+)
+print("Done Building XML Structure")
